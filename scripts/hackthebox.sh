bfccd4ba718214483d92fa5eee116f59c30816e0
CrimsonCloak <alexander.veldeman.999@gmail.com>
2024-10-28 12:14:50 +0100

    Add groups for custom user, add keyboard change feature, reboot at end of provisioning

--- scripts/hackthebox.sh
+++ scripts/hackthebox.sh
@@ -20,6 +20,8 @@ declare WORKSTATION_USER="hacker"
 
 declare WORKSTATION_USER_PASSWORD="hackerman"
 
+declare KEYBOARD_LOADOUT="be" # Change for desired keyboard loadout, eg. 'us', 'be',... (cat /usr/share/X11/xkb/rules/xorg.lst)
+
 #---------- Load utility functions --------------------------------------------
 
 source ${PROVISIONING_SCRIPTS}/util.sh
@@ -28,12 +30,23 @@ source ${PROVISIONING_SCRIPTS}/util.sh
 
 log "Starting server specific provisioning tasks on host ${HOSTNAME}..."
 
-log "Creating custom workstation user"
+log "Creating custom workstation user..."
 
 ensure_user_exists ${WORKSTATION_USER}
 # TO DO - set defaults for new user (home folder, shell!)
 
 echo ${WORKSTATION_USER}:${WORKSTATION_USER_PASSWORD} | chpasswd
+
+assign_groups ${WORKSTATION_USER} sudo
+
+log "Changing keyboard loadout..."
+
+set_keyboard_loadout ${KEYBOARD_LOADOUT}
+
+
+log "Rebooting virtual machine..."
+
+reboot
 
 
 
